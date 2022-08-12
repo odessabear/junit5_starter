931e1084a7d3886a7a0b7b07549a117022c36843
odessabear <bearodessa@gmail.com>
2022-08-12 17:52:07 +0300

    lessons 7-10

--- src/test/java/com/odebar/junit/TestLauncher.java
+++ src/test/java/com/odebar/junit/TestLauncher.java
@@ -1,9 +1,9 @@
 package com.odebar.junit;
 
-import org.junit.platform.engine.DiscoverySelector;
 import org.junit.platform.engine.discovery.DiscoverySelectors;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TagFilter;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
@@ -19,6 +19,9 @@ public class TestLauncher {
         LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder
                 .request()
                 .selectors(DiscoverySelectors.selectPackage("com.odebar.junit.service"))
+                .filters(
+                        TagFilter.excludeTags("login")
+                )
                 .build();
         launcher.execute(request, summaryGeneratingListener);
 
--- src/test/java/com/odebar/junit/service/UserServiceTest.java
+++ src/test/java/com/odebar/junit/service/UserServiceTest.java
@@ -2,106 +2,149 @@ package com.odebar.junit.service;
 
 import com.odebar.junit.dto.User;
 import org.hamcrest.MatcherAssert;
-import org.hamcrest.collection.IsEmptyCollection;
 import org.hamcrest.collection.IsMapContaining;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.TestMethodOrder;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@Tag("fast")
+@Tag("user")
+@TestInstance(Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.DisplayName.class)
 class UserServiceTest {
 
     private static final User IVAN = User.of(1, "Ivan", "123");
-    private static final User PETER = User.of(2, "Peter", "111");
+    private static final User PETR = User.of(2, "Petr", "111");
+
+//    @Rule
+//    ExpectedException
+
     private UserService userService;
 
     @BeforeAll
-    static void init() {
-        System.out.println("BeforeAll : ");
+    void init() {
+        System.out.println("Before all: " + this);
     }
 
     @BeforeEach
     void prepare() {
-        System.out.println("BeforeEach : " + this);
+        System.out.println("Before each: " + this);
         userService = new UserService();
     }
 
     @Test
-    void usersEmptyIfNoUsersAdded() {
-        System.out.println("Test1 : " + this);
-        List<User> users = userService.getAll();
+    @Order(1)
+    @DisplayName("users will be empty if no user added")
+    void usersEmptyIfNoUserAdded() {
+        System.out.println("Test 1: " + this);
+        var users = userService.getAll();
 
-        MatcherAssert.assertThat(users, IsEmptyCollection.empty());
-        assertTrue(users.isEmpty(), () -> "Users List should be empty");
+        MatcherAssert.assertThat(users, empty());
+        assertTrue(users.isEmpty(), "User list should be empty");
+//        input -> [box == func] -> actual output
     }
 
     @Test
-    void usersSizeIfUsersAdded() {
-        System.out.println("Test2 : " + this);
+    @Order(2)
+    void usersSizeIfUserAdded() {
+        System.out.println("Test 2: " + this);
         userService.add(IVAN);
-        userService.add(PETER);
+        userService.add(PETR);
 
-        List<User> users = userService.getAll();
+        var users = userService.getAll();
 
         assertThat(users).hasSize(2);
-        // assertEquals(2, users.size());
-    }
-
-    @Test
-    void loginSuccessIfUserExists() {
-        userService.add(IVAN);
-        Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
-
-        assertThat(maybeUser).isPresent();
-        maybeUser.ifPresent(user -> assertThat(user).isEqualTo(IVAN));
-
-//        assertTrue(maybeUser.isPresent());
-//        maybeUser.ifPresent(user -> assertEquals(IVAN, user));
+//        assertEquals(2, users.size());
     }
 
     @Test
     void usersConvertedToMapById() {
-        userService.add(IVAN, PETER);
+        userService.add(IVAN, PETR);
 
         Map<Integer, User> users = userService.getAllConvertedById();
 
         MatcherAssert.assertThat(users, IsMapContaining.hasKey(IVAN.getId()));
-
         assertAll(
-                () -> assertThat(users).containsKeys(IVAN.getId(), PETER.getId()),
-                () -> assertThat(users).containsValues(IVAN, PETER)
+                () -> assertThat(users).containsKeys(IVAN.getId(), PETR.getId()),
+                () -> assertThat(users).containsValues(IVAN, PETR)
         );
-
-    }
-
-    @Test
-    void loginFailedIfPasswordIsNotCorrect() {
-        userService.add(IVAN);
-        Optional<User> maybeUser = userService.login(IVAN.getUsername(), "dummy");
-
-        assertTrue(maybeUser.isEmpty());
-    }
-
-    @Test
-    void loginFailedIfUserDoesNotExists() {
-        userService.add(IVAN);
-        Optional<User> maybeUser = userService.login("dummy", IVAN.getPassword());
-
-        assertTrue(maybeUser.isEmpty());
     }
 
     @AfterEach
-    void deleteFromDatabase() {
-        System.out.println("AfterEach : " + this);
+    void deleteDataFromDatabase() {
+        System.out.println("After each: " + this);
     }
 
     @AfterAll
-    static void closeConnectionPool() {
-        System.out.println("AfterAll : ");
+    void closeConnectionPool() {
+        System.out.println("After all: " + this);
+    }
+
+    @Nested
+    @DisplayName("test user login functionality")
+    @Tag("login")
+    class LoginTest {
+
+        @Test
+        void loginFailIfPasswordIsNotCorrect() {
+            userService.add(IVAN);
+
+            var maybeUser = userService.login(IVAN.getUsername(), "dummy");
+
+            assertTrue(maybeUser.isEmpty());
+        }
+
+        @Test
+        void loginFailIfUserDoesNotExist() {
+            userService.add(IVAN);
+
+            var maybeUser = userService.login("dummy", IVAN.getPassword());
+
+            assertTrue(maybeUser.isEmpty());
+        }
+
+        @Test
+        void loginSuccessIfUserExists() {
+            userService.add(IVAN);
+
+            Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+
+            assertThat(maybeUser).isPresent();
+            maybeUser.ifPresent(user -> assertThat(user).isEqualTo(IVAN));
+//        assertTrue(maybeUser.isPresent());
+//        maybeUser.ifPresent(user -> assertEquals(IVAN, user));
+        }
+
+        @Test
+
+//    @org.junit.Test(expected = IllegalArgumentException.class)
+        void throwExceptionIfUsernameOrPasswordIsNull() {
+            assertAll(
+                    () -> {
+                        var exception = assertThrows(IllegalArgumentException.class, () -> userService.login(null, "dummy"));
+                        assertThat(exception.getMessage()).isEqualTo("username or password is null");
+                    },
+                    () -> assertThrows(IllegalArgumentException.class, () -> userService.login("dummy", null))
+            );
+        }
     }
 }
